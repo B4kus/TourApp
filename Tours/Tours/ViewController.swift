04d1e74b41e10ad230e4b6046cdb985e2410749d
Szymon Szysz <sszysz@netizens.pl>
2018-06-17 14:50:19 +0200

    Add start App

---
+++ Tours/Tours/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tours
+//
+//  Created by Szymon Szysz on 17.06.2018.
+//  Copyright © 2018 Szymon Szysz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
