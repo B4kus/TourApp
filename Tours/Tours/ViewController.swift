c1585aca7564f0ed8a90efaad39ed9359f96c3ae
Szymon Szysz <sszysz@netizens.pl>
2018-06-22 22:16:32 +0200

    Embed Navigation Controller and layout collection view with cell prototype

--- Tours/Tours/ViewController.swift
+++ Tours/Tours/ViewController.swift
@@ -7,19 +7,39 @@
 //
 
 import UIKit
+import Firebase
 
-class ViewController: UIViewController {
-
+class HomeController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+     
+        navigationItem.title = "Tours"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        collectionView?.backgroundColor = .white
+        
+        collectionView?.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "feed")
+        
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
     }
-
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "feed", for: indexPath)
+        
+        cell.backgroundColor = UIColor.red
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        return CGSize(width: view.frame.width - 25 , height: 226)
+    }
+    
 
 }
 
