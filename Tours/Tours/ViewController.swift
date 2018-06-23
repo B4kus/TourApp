1826abf0dbd218b2473b590a45397247368e3c2c
Szymon Szysz <sszysz@netizens.pl>
2018-06-23 20:12:10 +0200

    Add autolayout to cell content

--- Tours/Tours/ViewController.swift
+++ Tours/Tours/ViewController.swift
@@ -18,7 +18,7 @@ class HomeController: UICollectionViewController, UICollectionViewDelegateFlowLa
         navigationController?.navigationBar.prefersLargeTitles = true
         collectionView?.backgroundColor = .white
         
-        collectionView?.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "feed")
+        collectionView?.register(TourCell.self, forCellWithReuseIdentifier: "feed")
         
         
     }
@@ -31,15 +31,103 @@ class HomeController: UICollectionViewController, UICollectionViewDelegateFlowLa
         
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "feed", for: indexPath)
         
-        cell.backgroundColor = UIColor.red
         return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         
-        return CGSize(width: view.frame.width - 25 , height: 226)
+        return CGSize(width: view.frame.width, height: 226)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
     }
     
 
+}
+
+class TourCell: UICollectionViewCell {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+    }
+    
+    let thumbnailImageView: UIImageView = {
+        
+        let imageView = UIImageView()
+        imageView.backgroundColor = UIColor.blue
+        return imageView
+
+    }()
+    
+    let tourTimeLabel: UILabel = {
+       let tourTime = UILabel()
+        tourTime.backgroundColor = UIColor.red
+        return tourTime
+    }()
+    
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.backgroundColor = UIColor.purple
+        return label
+    }()
+    
+    let subTitle: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.backgroundColor = UIColor.green
+        return label
+    }()
+  
+    
+    func setupViews() {
+        
+        addSubview(thumbnailImageView)
+        addSubview(tourTimeLabel)
+        addSubview(titleLabel)
+        addSubview(subTitle)
+        
+        addConstraintsWithFormat(format: "H:|-16-[v0]-16-|", views: thumbnailImageView)
+        addConstraintsWithFormat(format: "V:|-16-[v0]-8-[v1(44)]-16-|", views: thumbnailImageView, tourTimeLabel)
+        addConstraintsWithFormat(format: "H:[v0(44)]-16-|", views: tourTimeLabel)
+        
+        addConstraint(.init(item: titleLabel, attribute: .top, relatedBy: .equal, toItem: thumbnailImageView, attribute: .bottom, multiplier: 1, constant: 8))
+        addConstraint(.init(item: titleLabel, attribute: .right, relatedBy: .equal, toItem: tourTimeLabel, attribute: .left, multiplier: 1, constant: -8))
+        addConstraint(.init(item: titleLabel, attribute: .left, relatedBy: .equal, toItem: thumbnailImageView, attribute: .left, multiplier: 1, constant: 0))
+        addConstraint(.init(item: titleLabel, attribute: .height, relatedBy: .equal, toItem: self, attribute: .height, multiplier: 0, constant: 20))
+        
+        addConstraint(.init(item: subTitle, attribute: .top, relatedBy: .equal, toItem: titleLabel, attribute: .bottom, multiplier: 1, constant: 8))
+        addConstraint(.init(item: subTitle, attribute: .right, relatedBy: .equal, toItem: tourTimeLabel, attribute: .left, multiplier: 1, constant: -8))
+        addConstraint(.init(item: subTitle, attribute: .left, relatedBy: .equal, toItem: titleLabel, attribute: .left, multiplier: 1, constant: 0))
+        addConstraint(.init(item: subTitle, attribute: .height, relatedBy: .equal, toItem: self, attribute: .height, multiplier: 0, constant: 20))
+        
+        
+        
+        
+    }
+    
+
+    
+    
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension UIView {
+    func addConstraintsWithFormat(format: String, views: UIView...) {
+        var viewsDictionary = [String: UIView]()
+        for (index, view) in views.enumerated() {
+            let key = "v\(index)"
+            view.translatesAutoresizingMaskIntoConstraints = false
+            viewsDictionary[key] = view
+            
+        }
+        
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: format, options: NSLayoutFormatOptions(), metrics: nil, views: viewsDictionary))
+    }
 }
 
