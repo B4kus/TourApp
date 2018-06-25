691c2ada51545373f5b6ed4cca3a0a7601d34ae7
Szymon Szysz <sszysz@netizens.pl>
2018-06-25 21:08:19 +0200

    Add Menu bar below navigation controller

---
+++ Tours/Tours/view/MenuBar.swift
@@ -0,0 +1,144 @@
+//
+//  MenuBar.swift
+//  Tours
+//
+//  Created by Szymon Szysz on 22.06.2018.
+//  Copyright © 2018 Szymon Szysz. All rights reserved.
+//
+
+import UIKit
+
+class MenuBar: UIView, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout{
+    
+    
+    
+    
+    lazy var collectonView: UICollectionView = {
+        
+        let layout = UICollectionViewFlowLayout()
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        cv.backgroundColor = UIColor.white
+        cv.translatesAutoresizingMaskIntoConstraints = false
+        cv.dataSource = self
+        cv.delegate = self
+        return cv
+        
+    }()
+    
+    let titleName = ["Featured", "Popluar", "Trending"]
+    
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        
+        collectonView.register(MenuCell.self, forCellWithReuseIdentifier: "title")
+        addSubview(collectonView)
+        addConstraintsWithFormat(format: "H:|[v0]|", views: collectonView)
+        addConstraintsWithFormat(format: "V:|[v0]|", views: collectonView)
+        
+        let selectedInedxPath = IndexPath(item: 0, section: 0)
+        collectonView.selectItem(at: selectedInedxPath, animated: false, scrollPosition: [])
+        
+        setupHorizontalBar()
+        
+        
+    }
+    
+    var horizontalBarLeftAnchorConstraints: NSLayoutConstraint?
+    func setupHorizontalBar() {
+        
+        let horizontalBar = UIView()
+        horizontalBar.backgroundColor = UIColor(red:0.31, green:0.63, blue:0.85, alpha:1.00)
+        horizontalBar.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(horizontalBar)
+        
+        horizontalBarLeftAnchorConstraints = horizontalBar.leftAnchor.constraintEqualToSystemSpacingAfter(self.leftAnchor, multiplier: 1)
+        horizontalBarLeftAnchorConstraints?.isActive = true
+        
+        
+        horizontalBar.bottomAnchor.constraintEqualToSystemSpacingBelow(self.bottomAnchor, multiplier: 1).isActive = true
+        horizontalBar.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: 1/4).isActive = true
+        horizontalBar.heightAnchor.constraint(equalToConstant: 4).isActive = true
+        
+        
+    }
+  
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let xValue = CGFloat(indexPath.item) * frame.width / 4
+        horizontalBarLeftAnchorConstraints?.constant = xValue
+        
+        UIView.animate(withDuration: 0.50) {
+            self.layoutIfNeeded()
+        }
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectonView.dequeueReusableCell(withReuseIdentifier: "title", for: indexPath) as! MenuCell
+        
+        cell.titleLabel.text = titleName[indexPath.item]
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: frame.width / 4, height: frame.height)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
+
+class MenuCell: BaseCell {
+    
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = UIColor.black
+        label.textAlignment = NSTextAlignment.center
+        return label
+    }()
+    
+    override var isHighlighted: Bool {
+        didSet {
+            
+            
+        }
+        
+    }
+    
+    override var isSelected: Bool {
+        didSet {
+            
+            
+        }
+        
+    }
+    
+    override func setupViews() {
+        super.setupViews()
+        addSubview(titleLabel)
+        
+        addConstraintsWithFormat(format: "H:|[v0]|", views: titleLabel)
+        addConstraintsWithFormat(format: "V:|[v0]|", views: titleLabel)
+        
+//        addConstraint(.init(item: titleLabel, attribute: .centerX, relatedBy: .equal, toItem: self, attribute: .centerX, multiplier: 1, constant: 0))
+//        addConstraint(.init(item: titleLabel, attribute: .centerY, relatedBy: .equal, toItem: self, attribute: .centerY, multiplier: 1, constant: 0))
+    
+    }
+    
+}
