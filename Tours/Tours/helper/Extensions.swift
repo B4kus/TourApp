3deef1024dcfc2d8c8a321f0476e636f9ec1490c
Szymon Szysz <sszysz@netizens.pl>
2018-06-25 21:07:42 +0200

    Move UIView extension to new file

---
+++ Tours/Tours/helper/Extensions.swift
@@ -0,0 +1,23 @@
+//
+//  Extensions.swift
+//  Tours
+//
+//  Created by Szymon Szysz on 24.06.2018.
+//  Copyright © 2018 Szymon Szysz. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    func addConstraintsWithFormat(format: String, views: UIView...) {
+        var viewsDictionary = [String: UIView]()
+        for (index, view) in views.enumerated() {
+            let key = "v\(index)"
+            view.translatesAutoresizingMaskIntoConstraints = false
+            viewsDictionary[key] = view
+            
+        }
+        
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: format, options: NSLayoutFormatOptions(), metrics: nil, views: viewsDictionary))
+    }
+}
