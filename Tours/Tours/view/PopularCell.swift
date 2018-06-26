030bcbf8d6717353239986f1cf611480e8ce4c47
Szymon Szysz <sszysz@netizens.pl>
2018-06-26 20:59:57 +0200

    Add two new cells calsses

---
+++ Tours/Tours/view/PopularCell.swift
@@ -0,0 +1,14 @@
+//
+//  PopularCell.swift
+//  Tours
+//
+//  Created by Szymon Szysz on 26.06.2018.
+//  Copyright © 2018 Szymon Szysz. All rights reserved.
+//
+
+import UIKit
+
+class PopularCell: FeedCell {
+    
+
+}
---
+++ Tours/Tours/view/TrendingCell.swift
@@ -0,0 +1,13 @@
+//
+//  TrendingCell.swift
+//  Tours
+//
+//  Created by Szymon Szysz on 26.06.2018.
+//  Copyright © 2018 Szymon Szysz. All rights reserved.
+//
+
+import UIKit
+
+class TrendingCell: FeedCell {
+    
+}
