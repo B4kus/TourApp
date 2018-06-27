a4b7b1581907af1af76541888a9432066590d572
Szymon Szysz <sszysz@netizens.pl>
2018-06-27 14:43:54 +0200

    Add detail view controller

---
+++ Tours/Tours/controller/DetailViewController.swift
@@ -0,0 +1,21 @@
+//
+//  DetailViewController.swift
+//  Tours
+//
+//  Created by Szymon Szysz on 27.06.2018.
+//  Copyright © 2018 Szymon Szysz. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = UIColor.red
+        
+    }
+
+
+}
